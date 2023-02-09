90034e38bc31d52bbf4917106a5e3a322f6b35c6
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-02-09 11:48:56 -0600

    empty pokemon array types

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -19,9 +19,17 @@ export class PokemonCardComponent implements OnInit {
     this.sharePokemonDataService.pokemon$.subscribe(pokemon => {
       this.pokemonName = pokemon.name
       this.pokemonSprite = pokemon.sprites.front_default
+      this.emptyTypesArray();
       pokemon.types.map(value => {
-        this.pokemonTypes.push(value.type.name)
+        this.pokemonTypes.push(value.type.name);
       })
     })
+  }
+
+  emptyTypesArray(): string[] {
+    while (this.pokemonTypes.length > 0) {
+      this.pokemonTypes.pop()
+    }
+    return this.pokemonTypes
   }
 }
